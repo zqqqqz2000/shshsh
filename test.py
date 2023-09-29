91635b3ef6b77f0487042704beb71f140e582814
zqqqqz2000 <zqqqqz2000@sina.cn>
2023-09-29 21:41:33 +0800

    fix: remove test

--- test.py
+++
@@ -1,4 +0,0 @@
-from shshsh import I, stdout
-
-for i in I >> "cat tests/case1/multiple_line":
-    print(i)
--- test1.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-import time
-
-
-for i in tqdm(range(1000)):
-    print(i)
-    time.sleep(0.1)
